4dbcdb02f05d8eee1ae24b188c6aea547c945a5c
thsc47 <thsc47@gmail.com>
2024-08-11 18:15:21 -0300

    feat: add `Notification` to handle with multiples exceptions

---
+++ domain/src/main/java/io/github/catalogo/admin/domain/validation/handler/Notification.java
@@ -0,0 +1,54 @@
+package io.github.catalogo.admin.domain.validation.handler;
+
+import io.github.catalogo.admin.domain.exceptions.DomainException;
+import io.github.catalogo.admin.domain.validation.Error;
+import io.github.catalogo.admin.domain.validation.ValidationHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification implements ValidationHandler {
+
+    private final List<Error> errors;
+
+    private Notification(List<Error> errors) {
+        this.errors = errors;
+    }
+
+    public static Notification create() {
+        return new Notification(new ArrayList<>());
+    }
+
+    public static Notification create(final Error anError) {
+        return new Notification(new ArrayList<>()).append(anError);
+    }
+
+    @Override
+    public Notification append(final Error anError) {
+        this.errors.add(anError);
+        return this;
+    }
+
+    @Override
+    public Notification append(final ValidationHandler aHandler) {
+        this.errors.addAll(aHandler.getErrors());
+        return this;
+    }
+
+    @Override
+    public Notification validate(final Validation aValidation) {
+        try {
+            aValidation.validate();
+        } catch (final DomainException ex) {
+            this.errors.addAll(ex.getErrors());
+        } catch (final Throwable t) {
+            this.errors.add(new Error(t.getMessage()));
+        }
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return this.errors;
+    }
+}
