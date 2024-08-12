9f51909c88797adc4481f45613c9fb527c34fcbf
thsc47 <thsc47@gmail.com>
2024-08-12 20:26:56 -0300

    feat: configure `h2 database` for general testing

--- infrastructure/src/test/java/io/github/catalogo/admin/infrastructure/ApplicationTests.java
+++ infrastructure/src/test/java/io/github/catalogo/admin/infrastructure/ApplicationTests.java
@@ -3,10 +3,13 @@ package io.github.catalogo.admin.infrastructure;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
+
 public class ApplicationTests {
 
     @Test
     public void testAppliticationRun() {
+        System.setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
