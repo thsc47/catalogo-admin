8792e013231d768a4b22888117077789146a7efb
thsc47 <thsc47@gmail.com>
2024-08-12 18:33:43 -0300

    feat: configure `production` and `development` profiles

--- infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/Main.java
@@ -4,9 +4,12 @@ import io.github.catalogo.admin.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
