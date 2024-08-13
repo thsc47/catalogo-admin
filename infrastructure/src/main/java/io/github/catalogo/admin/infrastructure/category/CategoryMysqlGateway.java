5946e9fe48fca52d5c3c0b8619228707f98836ef
thsc47 <thsc47@gmail.com>
2024-08-13 20:49:34 -0300

    feat: create `CategoryGateway` implementation

---
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/category/CategoryMysqlGateway.java
@@ -0,0 +1,46 @@
+package io.github.catalogo.admin.infrastructure.category;
+
+import io.github.catalogo.admin.domain.category.Category;
+import io.github.catalogo.admin.domain.category.CategoryGateway;
+import io.github.catalogo.admin.domain.category.CategoryId;
+import io.github.catalogo.admin.domain.category.CategorySearchQuery;
+import io.github.catalogo.admin.domain.pagination.Pagination;
+import io.github.catalogo.admin.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CategoryMysqlGateway implements CategoryGateway {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryMysqlGateway(final CategoryRepository aCategoryRepository) {
+        this.categoryRepository = aCategoryRepository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryId anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(CategoryId anId) {
+
+    }
+}
