bfdf5d699d7b3f7a926db0c3a9d95cae9e893750
thsc47 <thsc47@gmail.com>
2024-08-19 09:25:07 -0300

    feat: configure `category` endpoint swagger doc

--- infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/api/CategoryAPI.java
@@ -1,18 +1,35 @@
 package io.github.catalogo.admin.infrastructure.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping("categories")
+@Tag(name = "Categoris")
 public interface CategoryAPI {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE,
                  produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was throw"),
+            @ApiResponse(responseCode = "500", description = "An unexpected server error was throw")
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "List all categories pagineted")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "An invalid parameter was received"),
+            @ApiResponse(responseCode = "500", description = "An unexpected server error was throw")
+    })
     ResponseEntity<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
