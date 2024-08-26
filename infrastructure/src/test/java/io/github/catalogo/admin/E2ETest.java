599bc8d097509003361df1615d0500c4ae725822
thsc47 <thsc47@gmail.com>
2024-08-26 20:04:52 -0300

    test: add create category e2e test

--- infrastructure/src/test/java/io/github/catalogo/admin/E2ETest.java
+++ infrastructure/src/test/java/io/github/catalogo/admin/E2ETest.java
@@ -2,6 +2,7 @@ package io.github.catalogo.admin;
 
 import io.github.catalogo.admin.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,6 +11,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
+@AutoConfigureMockMvc
 @ActiveProfiles("test-e2e")
 @SpringBootTest(classes = {WebServerConfig.class})
 @ExtendWith(CleanUpExtension.class)
--- infrastructure/src/test/java/io/github/catalogo/admin/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/io/github/catalogo/admin/e2e/category/CategoryE2ETest.java
@@ -1,17 +1,41 @@
 package io.github.catalogo.admin.e2e.category;
 
 import io.github.catalogo.admin.E2ETest;
+import io.github.catalogo.admin.domain.category.CategoryId;
+import io.github.catalogo.admin.infrastructure.category.models.CategoryResponse;
+import io.github.catalogo.admin.infrastructure.category.models.CreateCategoryRequest;
+import io.github.catalogo.admin.infrastructure.category.persistence.CategoryRepository;
+import io.github.catalogo.admin.infrastructure.configuration.json.Json;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.wildfly.common.Assert.assertNotNull;
+import static org.wildfly.common.Assert.assertTrue;
+
 @E2ETest
+@Disabled
 @Testcontainers
 public class CategoryE2ETest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Container
     private static final MySQLContainer MYSQL_CONTAINER
@@ -24,4 +48,53 @@ public class CategoryE2ETest {
     public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
         registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
     }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+        assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = retrieveACategory(actualId.getValue());
+
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.active());
+        assertNotNull(actualCategory.createdAt());
+        assertNotNull(actualCategory.updatedAt());
+        assertNull(actualCategory.deletedAt());
+    }
+
+    private CategoryId givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+        final var aRequestBody = new CreateCategoryRequest(aName, aDescription, isActive);
+
+        final var actualId = mvc.perform(post("/categories")
+                        .contentType(APPLICATION_JSON)
+                        .content(Json.writeValueAsString(aRequestBody)))
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/", "");
+
+        return CategoryId.from(actualId);
+    }
+
+    private CategoryResponse retrieveACategory(final String anId) throws Exception {
+
+        final var json = mvc.perform(get("/categories/" + anId)
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        return Json.readValue(json, CategoryResponse.class);
+    }
+
+
 }
