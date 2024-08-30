f208b6e2c2b746b0cc85b08bd3605e2794784d13
thsc47 <thsc47@gmail.com>
2024-08-29 21:29:47 -0300

    test: add create `Genre`

---
+++ domain/src/test/java/domain/genre/GenreTest.java
@@ -0,0 +1,25 @@
+package domain.genre;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GenreTest {
+    @Test
+    public void givenValidParams_whenCallNewGenre_shouldInstantiateAGenre() {
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        assertNotNull(actualGenre);
+        assertNotNull(actualGenre.getId());
+        assertEquals(expectedName, actualGenre.getName());
+        assertEquals(expectedIsActive, actualGenre.isActive());
+        assertEquals(expectedCategories, actualGenre.getCategories().size());
+        assertNotNull(actualGenre.getCreatedAt());
+        assertNotNull(actualGenre.getUpdatedAt());
+        assertNull(actualGenre.getDeletedAt());
+    }
+}
