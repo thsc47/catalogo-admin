fe81a6024d17526c3c108d5dbd99ffa3977c32fb
thsc47 <thsc47@gmail.com>
2024-09-12 08:37:20 -0300

    test: add more scenarios for UpdateGenreUseCaseTests

--- application/src/test/java/io/github/catalogo/admin/application/genre/update/UpdateGenreUseCaseTests.java
+++ application/src/test/java/io/github/catalogo/admin/application/genre/update/UpdateGenreUseCaseTests.java
@@ -2,6 +2,7 @@ package io.github.catalogo.admin.application.genre.update;
 
 import io.github.catalogo.admin.domain.category.CategoryGateway;
 import io.github.catalogo.admin.domain.category.CategoryId;
+import io.github.catalogo.admin.domain.exceptions.NotificationException;
 import io.github.catalogo.admin.domain.genre.Genre;
 import io.github.catalogo.admin.domain.genre.GenreGateway;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,10 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdateGenreUseCaseTests {
@@ -64,7 +63,7 @@ public class UpdateGenreUseCaseTests {
 
         verify(genreGateway).findById(eq(expectedId));
 
-        verify(genreGateway)).update(argThat(aUpdatedGenre ->
+        verify(genreGateway).update(argThat(aUpdatedGenre ->
                 Objects.equals(expectedId, aUpdatedGenre.getId())
                         && Objects.equals(expectedName, aUpdatedGenre.getName())
                         && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
@@ -73,6 +72,189 @@ public class UpdateGenreUseCaseTests {
                         && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
                         && Objects.isNull(aUpdatedGenre.getDeletedAt())
         ));
+    }
+
+    @Test
+    public void givenAValidCommandWithCategories_whenCallsUpdateGenre_shouldReturnGenreId() {
+        // given
+        final var aGenre = Genre.newGenre("acao", true);
+
+        final var expectedId = aGenre.getId();
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(
+                CategoryId.from("123"),
+                CategoryId.from("456")
+        );
+
+        final var aCommand = UpdateGenreCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedIsActive,
+                asString(expectedCategories)
+        );
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+        when(categoryGateway.existsByIds(any()))
+                .thenReturn(expectedCategories);
+
+        when(genreGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        assertNotNull(actualOutput);
+        assertEquals(expectedId.getValue(), actualOutput.id());
+
+        verify(genreGateway, times(1)).findById(eq(expectedId));
+
+        verify(categoryGateway, times(1)).existsByIds(eq(expectedCategories));
+
+        verify(genreGateway).update(argThat(aUpdatedGenre ->
+                Objects.equals(expectedId, aUpdatedGenre.getId())
+                        && Objects.equals(expectedName, aUpdatedGenre.getName())
+                        && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
+                        && Objects.equals(expectedCategories, aUpdatedGenre.getCategories())
+                        && Objects.equals(aGenre.getCreatedAt(), aUpdatedGenre.getCreatedAt())
+                        && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
+                        && Objects.isNull(aUpdatedGenre.getDeletedAt())
+        ));
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveGenre_whenCallsUpdateGenre_shouldReturnGenreId() {
+        // given
+        final var aGenre = Genre.newGenre("acao", true);
+
+        final var expectedId = aGenre.getId();
+        final var expectedName = "Ação";
+        final var expectedIsActive = false;
+        final var expectedCategories = List.<CategoryId>of();
+
+        final var aCommand = UpdateGenreCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedIsActive,
+                asString(expectedCategories)
+        );
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+        when(genreGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        assertTrue(aGenre.isActive());
+        assertNull(aGenre.getDeletedAt());
+
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        assertNotNull(actualOutput);
+        assertEquals(expectedId.getValue(), actualOutput.id());
+
+        verify(genreGateway).findById(eq(expectedId));
+
+        verify(genreGateway).update(argThat(aUpdatedGenre ->
+                Objects.equals(expectedId, aUpdatedGenre.getId())
+                        && Objects.equals(expectedName, aUpdatedGenre.getName())
+                        && Objects.equals(expectedIsActive, aUpdatedGenre.isActive())
+                        && Objects.equals(expectedCategories, aUpdatedGenre.getCategories())
+                        && Objects.equals(aGenre.getCreatedAt(), aUpdatedGenre.getCreatedAt())
+                        && aGenre.getUpdatedAt().isBefore(aUpdatedGenre.getUpdatedAt())
+                        && Objects.nonNull(aUpdatedGenre.getDeletedAt())
+        ));
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallsUpdateGenre_shouldReturnNotificationException() {
+        // given
+        final var aGenre = Genre.newGenre("acao", true);
+
+        final var expectedId = aGenre.getId();
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryId>of();
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = UpdateGenreCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedIsActive,
+                asString(expectedCategories)
+        );
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+        // when
+        final var actualException = assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        // then
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(genreGateway).findById(eq(expectedId));
+
+        verify(categoryGateway, never()).existsByIds(any());
+
+        verify(genreGateway, never()).update(any());
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallsUpdateGenreAndSomeCategoriesDoesNotExists_shouldReturnNotificationException() {
+        // given
+        final var filmes = CategoryId.from("123");
+        final var series = CategoryId.from("456");
+        final var documentarios = CategoryId.from("789");
+
+        final var aGenre = Genre.newGenre("acao", true);
+
+        final var expectedId = aGenre.getId();
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(filmes, series, documentarios);
+
+        final var expectedErrorCount = 2;
+        final var expectedErrorMessageOne = "Some categories could not be found: 456, 789";
+        final var expectedErrorMessageTwo = "'name' should not be null";
+
+        final var aCommand = UpdateGenreCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedIsActive,
+                asString(expectedCategories)
+        );
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+        when(categoryGateway.existsByIds(any()))
+                .thenReturn(List.of(filmes));
+
+        // when
+        final var actualException = assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        // then
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessageOne, actualException.getErrors().get(0).message());
+        assertEquals(expectedErrorMessageTwo, actualException.getErrors().get(1).message());
+
+        verify(genreGateway).findById(eq(expectedId));
+
+        verify(categoryGateway).existsByIds(eq(expectedCategories));
+
+        verify(genreGateway, never()).update(any());
     }
 
     private List<String> asString(final List<CategoryId> ids) {
