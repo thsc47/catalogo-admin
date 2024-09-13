406c60abb05bdb9b2e254586ba77c09c7d094b20
thsc47 <thsc47@gmail.com>
2024-09-13 08:44:54 -0300

    test: add deleteGenre scenarios

---
+++ application/src/test/java/io/github/catalogo/admin/application/genre/delete/DeleteGenreUseCaseTest.java
@@ -0,0 +1,78 @@
+package io.github.catalogo.admin.application.genre.delete;
+
+import io.github.catalogo.admin.application.UseCaseTest;
+import io.github.catalogo.admin.domain.genre.Genre;
+import io.github.catalogo.admin.domain.genre.GenreGateway;
+import io.github.catalogo.admin.domain.genre.GenreId;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+
+public class DeleteGenreUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultDeleteGenreUseCase useCase;
+
+    @Mock
+    private GenreGateway genreGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(genreGateway);
+    }
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldDeleteGenre() {
+        // given
+        final var aGenre = Genre.newGenre("Ação", true);
+
+        final var expectedId = aGenre.getId();
+
+        doNothing().when(genreGateway).deleteById(any());
+
+        // when
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        verify(genreGateway).deleteById(expectedId);
+    }
+
+    @Test
+    public void givenAnInvalidGenreId_whenCallsDeleteGenre_shouldBeOk() {
+        // given
+        final var expectedId = GenreId.from("123");
+
+        doNothing().when(genreGateway).deleteById(any());
+
+        // when
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // when
+        verify(genreGateway).deleteById(expectedId);
+    }
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenreAndGatewayThrowsUnexpectedError_shouldReceiveException() {
+        // given
+        final var aGenre = Genre.newGenre("Ação", true);
+        final var expectedId = aGenre.getId();
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(genreGateway).deleteById(any());
+
+        // when
+        assertThrows(IllegalStateException.class, () -> {
+            useCase.execute(expectedId.getValue());
+        });
+
+        // when
+        verify(genreGateway).deleteById(expectedId);
+    }
+}
