7fe0e732000c7fee62ab2432d2ea0c55a1f17d19
thsc47 <thsc47@gmail.com>
2024-09-13 08:34:15 -0300

    test: create `UseCaseTest` wrapper

---
+++ application/src/test/java/io/github/catalogo/admin/application/UseCaseTest.java
@@ -0,0 +1,21 @@
+package io.github.catalogo.admin.application;
+
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.reset;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class UseCaseTest implements BeforeEachCallback {
+
+    @Override
+    public void beforeEach(final ExtensionContext context) {
+        reset(getMocks().toArray());
+    }
+
+    protected abstract List<Object> getMocks();
+}
