640ec406f09ca8779951748ea938b098b6429b16
thsc47 <thsc47@gmail.com>
2024-09-16 17:32:14 -0300

    fix: add import in `GenreRepository`

--- infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/persistence/GenreRepository.java
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/persistence/GenreRepository.java
@@ -1,4 +1,6 @@
 package io.github.catalogo.admin.infrastructure.genre.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface GenreRepository extends JpaRepository<GenreJpaEntity, String> {
 }
