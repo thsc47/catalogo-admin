eeb9fdb2c5a9dd37eea05887e54ca4045cd0639a
thsc47 <thsc47@gmail.com>
2024-09-16 17:42:01 -0300

    feat: implements `GenreMySQLGateway`

--- infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGateway.java
@@ -1,4 +1,39 @@
 package io.github.catalogo.admin.infrastructure.genre;
 
-public class GenreMySQLGateway {
+import io.github.catalogo.admin.domain.genre.Genre;
+import io.github.catalogo.admin.domain.genre.GenreGateway;
+import io.github.catalogo.admin.domain.genre.GenreId;
+import io.github.catalogo.admin.domain.pagination.Pagination;
+import io.github.catalogo.admin.domain.pagination.SearchQuery;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class GenreMySQLGateway implements GenreGateway {
+
+    @Override
+    public Genre create(Genre aGenre) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(GenreId anId) {
+
+    }
+
+    @Override
+    public Optional<Genre> findById(GenreId anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Genre update(Genre aGenre) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Genre> findAll(SearchQuery aQuery) {
+        return null;
+    }
 }
