d32a1cff0359e71db2c7427dc01247f028d395b8
thsc47 <thsc47@gmail.com>
2024-09-19 18:04:42 -0300

    feat: refactor in GenreMySQLGateway

--- infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGateway.java
+++ infrastructure/src/main/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGateway.java
@@ -5,16 +5,25 @@ import io.github.catalogo.admin.domain.genre.GenreGateway;
 import io.github.catalogo.admin.domain.genre.GenreId;
 import io.github.catalogo.admin.domain.pagination.Pagination;
 import io.github.catalogo.admin.domain.pagination.SearchQuery;
+import io.github.catalogo.admin.infrastructure.genre.persistence.GenreJpaEntity;
+import io.github.catalogo.admin.infrastructure.genre.persistence.GenreRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
 public class GenreMySQLGateway implements GenreGateway {
 
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
     @Override
-    public Genre create(Genre aGenre) {
-        return null;
+    public Genre create(final Genre aGenre) {
+        return save(aGenre);
     }
 
     @Override
@@ -35,5 +44,10 @@ public class GenreMySQLGateway implements GenreGateway {
     @Override
     public Pagination<Genre> findAll(SearchQuery aQuery) {
         return null;
+    }
+
+    private Genre save(final Genre aGenre) {
+        return this.genreRepository.save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
     }
 }
