f6be3e25ceb451764ebd11fbe10d34dfc91b82b2
thsc47 <thsc47@gmail.com>
2024-09-20 22:17:49 -0300

    style: remove unused imports

--- infrastructure/src/test/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGatewayTest.java
+++ infrastructure/src/test/java/io/github/catalogo/admin/infrastructure/genre/GenreMySQLGatewayTest.java
@@ -10,7 +10,6 @@ import io.github.catalogo.admin.infrastructure.genre.persistence.GenreRepository
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.wildfly.common.Assert;
 
 import java.util.List;
 
